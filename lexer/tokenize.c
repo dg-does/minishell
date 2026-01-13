c25c54026e19f83debf3184907688347f71a9e00
Dimple Gulrajani <digulraj@c0c4c2.42berlin.de>
2026-01-13 15:40:53 +0100

    testing

--- lexer/tokenize.c
+++ lexer/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: digulraj <digulraj@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 11:43:33 by digulraj          #+#    #+#             */
-/*   Updated: 2026/01/13 14:48:20 by digulraj         ###   ########.fr       */
+/*   Updated: 2026/01/13 15:32:45 by digulraj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,13 @@ void	add_token(t_token **head, char *value)
 	return (new_token);
 }
 
+int	is_quote(char c)
+{
+	if (c == '"' || c == '\'')
+		return (1);
+	else
+		return (0);
+}
 
 t_token	tokenize(char *input)
 {
@@ -31,27 +38,24 @@ t_token	tokenize(char *input)
 	char	buffer[1024];
 	int		i;
 	int		j;
-	int		in_quotes;
+	char	quote;
 
 	*head = NULL;
 	i = 0;
-	in_quotes = 0;
 	while (input[i])
 	{
-		while (input[i] && isspace(input[i]) && !in_quotes)
+		while (input[i] && isspace(input[i]))
 			i++;
 		if (!input[i])
 			break ;
 		j = 0;
-		if (input[i] == '"')
+		if (is_quote(input[i]))
 		{
-			i++;
-			in_quotes = 1;
-			while (input[i] && input[i] != '"')
+			quote = input[i++];
+			while (input[i] && !is_quote(input[i]))
 				buffer[j++] = input[i++];
-			if (input[i] == '"')
+			if (input[i] == quote)
 				i++;
-			in_quotes = 0;
 			buffer[j] = '\0';
 			add_token(&head, ft_strdup(buffer));
 		}
@@ -63,7 +67,8 @@ t_token	tokenize(char *input)
 		}
 		else
 		{
-			while (input[i] && !isspace(input[i]) && !is_special(input[i]))
+			while (input[i] && !isspace(input[i]) && !is_special(input[i]) && 
+				!is_quote(input[i]))
 				buffer[j++] = input[i++];
 			buffer[j] = '\0';
 			if (j > 0)
@@ -75,12 +80,11 @@ t_token	tokenize(char *input)
 /*
 Still need to:
 * if there's no close quotation mark
-* single quotes
 * change the boolean flag in the struct to organise what type of token it is
-* handle >> also in special characters, rn it's only 1 char
+* handle ">> also in special characters, rn it's only 1 char. bash handles >> so we need to save it as a token
+* handle escape characters using '/' the way bash does
 *implement isspace and is_special
 */
-
 
 
 int	main(void)
