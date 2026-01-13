46343334cde322ad68cfb9f902c32998d5fe7d04
Felix Greiff <felix@follmer-greiff.de>
2026-01-13 15:34:14 +0100

    still testing

--- lexer.h
+++ lexer.h
@@ -3,22 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: digulraj <digulraj@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: fgreiff <fgreiff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 11:44:07 by digulraj          #+#    #+#             */
-/*   Updated: 2026/01/13 11:48:58 by digulraj         ###   ########.fr       */
+/*   Updated: 2026/01/13 15:30:56 by fgreiff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <readline.h>
+#ifndef LEXER_H
+# define LEXER_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+# include <readline.h>
 
 
 typedef struct s_token
 {
 	char			*value;
 	struct s_token	*next;
+	int				word;
+	int				redirect;
+	int				s_quotes;
+	int				d_quotes;
+	int				pipe;
+	int				expand;
 } t_token;
+
+#endif
