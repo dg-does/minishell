558b0125e0f3780f25d01eda24e7365ab0cd4416
Dimple Gulrajani <digulraj@c1b2c2.42berlin.de>
2026-02-24 14:48:26 +0100

    Added signal handling

--- src/minishell.h
+++ src/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: felixgreiff <felixgreiff@student.42.fr>    +#+  +:+       +#+        */
+/*   By: digulraj <digulraj@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 15:45:38 by felixgreiff       #+#    #+#             */
-/*   Updated: 2026/02/19 17:25:34 by felixgreiff      ###   ########.fr       */
+/*   Updated: 2026/02/24 13:45:59 by digulraj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "lexer.h"
 # include "built_in.h"
 # include "execution.h"
+# include <signal.h>
 
 typedef struct s_minishell
 {
@@ -23,6 +24,11 @@ typedef struct s_minishell
 	int		last_exit_status;
 }	t_minishell;
 
-void array_free(char **str);
+//Global variable used for passing signals
+extern volatile sig_atomic_t	g_sig;
+
+void	reset_signals(void);
+void	setup_signals(void);
+void	array_free(char **str);
 
 #endif
